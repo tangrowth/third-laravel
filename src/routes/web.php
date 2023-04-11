f95a1cf1ce8d9357dca7b9053b8e6714d95b8b6e
tangrowth <to.chinatsu@icloud.com>
2023-04-11 12:11:41 +0900

    2-5 認証機能完了

---
+++ src/routes/web.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::middleware('auth')->group(function () {
+  Route::get('/', [AuthController::class, 'index']);
+});
